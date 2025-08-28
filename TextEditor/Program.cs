cfcd606782ffdcd6b456ebe43b2fb01044f532c3
Mateus Pereira da Silva <mateusufc1@live.com>
2025-08-28 16:21:25 -0300

    feat: menu, open, save, edit

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,19 +1,71 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
-
-
     class Program
     {
-        static void Main(string[] args) 
-        { 
-            
+        static void Main(string[] args)
+        {
+            Menu();
         }
-        
-        static void Menu(2)
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Open File");
+            Console.WriteLine("2 - Create new File");
+            Console.WriteLine("0 - Exit");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+
+            }
+        }
+        static void Open()
         {
 
         }
+
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Type you text. (Press ESQ to leave)");
+            Console.WriteLine("----------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("How is the path to save the file?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.WriteLine(text);
+            }
+
+            Console.WriteLine($"File saved in {path}.");
+            Menu();
+        }
+
+        
     }
+    
 }
