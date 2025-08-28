1c268b7a0afca9889bae05972843c79abda4aefc
Mateus Pereira da Silva <mateusufc1@live.com>
2025-08-28 15:53:46 -0300

    init

---
+++ TextEditor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextEditor
+{
+
+
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            
+        }
+        
+        static void Menu(2)
+        {
+
+        }
+    }
+}
